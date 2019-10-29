c811ff775dd4d399c08f04e5e69d6f45930ae0ea
mateusdoimo <mateusfernandesdoimo@gmail.com>
2019-10-29 18:03:35 -0200

    Funções de copiar, colar, recortar, desfazer e refazer

--- Editor/src/editor/Editor.java
+++ Editor/src/editor/Editor.java
@@ -16,6 +16,7 @@ public class Editor {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JEditor e = new JEditor(); 
     }
     
 }
--- Editor/src/editor/JEditor.java
+++ Editor/src/editor/JEditor.java
@@ -5,23 +5,44 @@
  */
 package editor;
 
+import java.awt.Color;
+import java.awt.Event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.KeyStroke;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.UndoableEditEvent;
+import javax.swing.event.UndoableEditListener;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.swing.plaf.metal.OceanTheme;
+import javax.swing.text.Document;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 
 /**
  *
@@ -29,25 +50,54 @@ import javax.swing.plaf.metal.OceanTheme;
  */
 public class JEditor extends JFrame implements ActionListener{
     //Componentes de texto
-    JTextArea t;
+    private final JTextArea t;
     
     //Frame
-    JFrame f;
-    
+    private final JFrame f;
+    private final UndoManager undo = new UndoManager();
     //construtor
-    JEditor(){
+    public JEditor(){
+        
         f = new JFrame("Editor de texto");
+        
         try{
             UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
             
             MetalLookAndFeel.setCurrentTheme(new OceanTheme());
         } catch (Exception e){}
         
-        t = new JTextArea();
+        t = new JTextArea(10,20);
+        
+        //
+        KeyStroke undoKeyStroke = KeyStroke.getKeyStroke(
+            KeyEvent.VK_Z, Event.CTRL_MASK);
+        KeyStroke redoKeyStroke = KeyStroke.getKeyStroke(
+            KeyEvent.VK_Y, Event.CTRL_MASK);
+
+        UndoManager undoManager = new UndoManager();
+        
+        Document document = t.getDocument();
+        document.addUndoableEditListener(new UndoableEditListener() {
+            @Override
+            public void undoableEditHappened(UndoableEditEvent e) {
+                undoManager.addEdit(e.getEdit());
+            }
+        });
+        //
+        
         
         JMenuBar mb = new JMenuBar();
         
         JMenu itemArq = new JMenu("Arquivo");
+        
+        itemArq.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/file.png")));
+        itemArq.setText("Arquivo");
+        itemArq.setFocusable(false);
+//        itemArq.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);;
+//        itemArq.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        itemArq.setHorizontalAlignment(SwingConstants.LEFT);
+        itemArq.setBorder(new EmptyBorder(5,5,5,5));
+        mb.setBackground(Color.white);
         
         JMenuItem btnNovo = new JMenuItem("Novo");
         JMenuItem btnAbrir = new JMenuItem("Abrir");
@@ -63,6 +113,7 @@ public class JEditor extends JFrame implements ActionListener{
         
         JMenu itemEdit = new JMenu("Editar");
         
+
         JMenuItem btnRecortar = new JMenuItem("Recortar");
         JMenuItem btnCopiar = new JMenuItem("Copiar");
         JMenuItem btnColar = new JMenuItem("Colar");
@@ -72,8 +123,43 @@ public class JEditor extends JFrame implements ActionListener{
         btnRecortar.addActionListener(this);
         btnCopiar.addActionListener(this);
         btnColar.addActionListener(this);
-        btnDesfazer.addActionListener(this);
-        btnRefazer.addActionListener(this);
+//        btnDesfazer.addActionListener(this);
+        //btnRefazer.addActionListener(this);
+        
+        // Add ActionListeners
+        btnDesfazer.addActionListener((ActionEvent e) -> {
+            try {
+                undoManager.undo();
+            } catch (CannotUndoException cue) {}
+        });
+        btnRefazer.addActionListener((ActionEvent e) -> {
+            try {
+                undoManager.redo();
+            } catch (CannotRedoException cre) {}
+        });
+
+        // Map undo action
+        t.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+                .put(undoKeyStroke, "undoKeyStroke");
+        t.getActionMap().put("undoKeyStroke", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    undoManager.undo();
+                 } catch (CannotUndoException cue) {}
+            }
+        });
+        // Map redo action
+        t.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
+                .put(redoKeyStroke, "redoKeyStroke");
+        t.getActionMap().put("redoKeyStroke", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    undoManager.redo();
+                 } catch (CannotRedoException cre) {}
+            }
+        });
         
         itemEdit.add(btnRecortar);
         itemEdit.add(btnCopiar);
@@ -87,12 +173,40 @@ public class JEditor extends JFrame implements ActionListener{
         
         mb.add(itemArq);
         mb.add(itemEdit);
-        mb.add(itemFechar);
+//        mb.add(itemFechar);
+        
+        
+        JScrollPane scroll = new JScrollPane (t);
+        scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
         
         f.setJMenuBar(mb);
-        f.add(t);
-        f.setSize(500, 500);
-        f.show();
+        f.add(scroll);
+        f.setSize(700, 600);
+        
+        t.setColumns(20);
+        t.setFont(new java.awt.Font("Monospaced", 0, 14)); // NOI18N
+        t.setLineWrap(true);
+        t.setRows(5);
+        t.setBorder(new EmptyBorder(10, 10, 10, 10));
+        
+        //f.setIconImage(iconeTitulo);
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+              
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e); //To change body of generated methods, choose Tools | Templates.
+                int ans = JOptionPane.showConfirmDialog(rootPane, "Save Changes ?", "Confirm", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                if (ans == JOptionPane.YES_OPTION) {
+                    //saveChanges();
+                }
+            }
+
+        });
+        
+        f.setVisible(true);
     }
     
     public void actionPerformed(ActionEvent e)     { 
@@ -100,13 +214,13 @@ public class JEditor extends JFrame implements ActionListener{
   
         switch (s) {
             case "Recortar":
-                t.recortar();
+                t.cut();
                 break;
             case "Copiar":
-                t.copiar() ;
+                t.copy() ;
                 break;
             case "Colar":
-                t.colar();
+                t.paste();
                 break;
             case "Salvar":
                 {
@@ -191,11 +305,5 @@ public class JEditor extends JFrame implements ActionListener{
             default:
                 break;
         }
-    } 
-  
-    // Main class 
-    public static void main(String args[]) 
-    { 
-        JEditor e = new JEditor(); 
     } 
 }
